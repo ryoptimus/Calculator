fd0d6c2c5292cd30c2cc65eba70521e70560b187
Ryo Novoa <ryosnovoa@gmail.com>
2024-09-07 13:55:45 -0700

    added images and stylesheets

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,19 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+import javafx.stage.StageStyle;
+
 public class Calculator extends Application {
     @Override
-    public void start(Stage stage) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindowInterface.fxml"));
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("CalculatorInterface.fxml")));
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setTitle("Calculator");
         stage.show();
     }
 
--- src/CalculatorController.java
+++ src/CalculatorController.java
@@ -1,2 +1,10 @@
-public class CalculatorController {
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CalculatorController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
